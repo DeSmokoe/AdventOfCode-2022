9949bd5da41730d1476d0f3c7eb1df029dd7e058
DeSmokoe <alexvandromme@gmail.com>
2022-12-08 20:09:41 +0100

    Day 5 Done

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -48,34 +48,42 @@ def dataprep(data):
     return table1, table2
 
 
-def move_crates_9000(position, instructions):
+def move_crates(position, instructions, type_crane):
+    # For each instruction
     for i in range(len(instructions)):
-        for j in range(instructions["Amount"][i]):
-            dest_col = instructions["Destination"][i]
-            ori_col = instructions["Origin"][i]
+        # for the right amount of crates
 
+        dest_col = instructions["Destination"][i]
+        ori_col = instructions["Origin"][i]
+        if type_crane == "9001":
+            ori_row_safe = len(position) - len(position[position[ori_col] != " "])
+
+        for j in range(instructions["Amount"][i]):
+            # Declarations
             dest_row = len(position)-len(position[position[dest_col] != " "]) - 1
-            ori_row = len(position)-len(position[position[ori_col] != " "])
+
+            if type_crane == "9000":
+                ori_row = len(position)-len(position[position[ori_col] != " "])
+            elif type_crane == "9001":
+                ori_row = ori_row_safe + instructions["Amount"][i] - j - 1
 
             if dest_row == -1:
-                dest_row = 0
+                dest_row += 1
                 ori_row += 1
+                if type_crane == "9001":
+                    ori_row_safe += 1
 
                 # Create a new row at the top of the table
                 position.loc[-1] = [" "] * len(position.columns)
                 position.index = position.index + 1
                 position = position.sort_index()
 
-            position[dest_col][dest_row] = \
-                position[ori_col][ori_row]
-
+            # Move to new position
+            position[dest_col][dest_row] = position[ori_col][ori_row]
+            # Remove original
             position[ori_col][ori_row] = " "
 
     return position
-
-
-def move_crates_9001(position, instructions):
-    pass
 
 
 def find_top_crates(position):
@@ -89,5 +97,7 @@ def find_top_crates(position):
     print(top_crates)
 
 
-find_top_crates(move_crates_9000(dataprep("Test.txt")[0], dataprep("Test.txt")[1]))
-find_top_crates(move_crates_9000(dataprep("Puzzle.txt")[0], dataprep("Puzzle.txt")[1]))
+# find_top_crates(move_crates(dataprep("Test.txt")[0], dataprep("Test.txt")[1], "9000"))
+# find_top_crates(move_crates(dataprep("Puzzle.txt")[0], dataprep("Puzzle.txt")[1], "9000"))
+# find_top_crates(move_crates(dataprep("Test.txt")[0], dataprep("Test.txt")[1], "9001"))
+find_top_crates(move_crates(dataprep("Puzzle.txt")[0], dataprep("Puzzle.txt")[1], "9001"))
