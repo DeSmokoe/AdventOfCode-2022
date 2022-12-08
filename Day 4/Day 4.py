285a765bd3b03e80eaa3ce07ec16dbf20af4dc5e
DeSmokoe <alexvandromme@gmail.com>
2022-12-08 15:10:38 +0100

    Day 4 Complete

--- Day 4/Day 4.py
+++ Day 4/Day 4.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 def readfile(filename):
     with open(filename, "r") as f:
         return f.read()
@@ -9,19 +6,39 @@ def readfile(filename):
 def dataprep(data):
     my_file = readfile(data)
     array = my_file.split("\n")
-    array = pd.Series(array)
 
-    # split the array in 2 at every comma and "-"
-    array = array.str.split(",", expand=True)
+    # split array at every ,
+    array = [x.split(",") for x in array]
 
-    # split the array in 2 at every "-"
-    array = array.stack().str.split("-", expand=True).unstack()
+    # split every array at the -
+    array = [[x[0].split("-"), x[1].split("-")] for x in array]
+
+    # turn the strings into ints
+    array = [[[int(x[0]), int(x[1])] for x in y] for y in array]
+
+    # turn the array into a range of numbers from the first to the second number
+    array = [[range(x[0][0], x[0][1] + 1), range(x[1][0], x[1][1] + 1)] for x in array]
+
+    return array
 
 
-    # expand the array to a list starting from the first int to the last
-    #array = array.apply(lambda x: list(range(int(x[0]), int(x[1]) + 1)), axis=1)
+def calculate_overlaps(array):
+    # find the overlap between the two ranges
+    overlap = [set(x[0]).intersection(set(x[1])) for x in array]
 
-    print(array)
+    # find the length of the overlap
+    overlap = [len(x) for x in overlap]
+
+    # find the overlaps where the overlap is greater or equal to the length of the first or second range
+    amount_full = [x >= len(array[i][0]) or x >= len(array[i][1]) for i, x in enumerate(overlap)]
+
+    # find all overlaps
+    amount_partial = [x > 0 for x in overlap]
+
+    # return the number of overlaps
+    return sum(amount_full), sum(amount_partial)
 
 
-dataprep("Test.txt")
+print(calculate_overlaps(dataprep("Test.txt")))
+print(calculate_overlaps(dataprep("Puzzle.txt")))
+
