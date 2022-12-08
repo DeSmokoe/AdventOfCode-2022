9bcf6d5238a2cc417c798c2d556a153eeb2f27ca
DeSmokoe <alexvandromme@gmail.com>
2022-12-08 18:38:29 +0100

    Changing columns implemented
    ToDo: create extra rows

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -28,6 +28,12 @@ def dataprep(data):
     # keep only every fourth column, starting from the second column in table1
     table1 = table1.iloc[:, 1::4]
 
+    # rename the columns in table1 to ascending numbers starting from 1
+    table1.columns = range(1, len(table1.columns) + 1)
+
+    # remove the last row in table1
+    table1 = table1.iloc[:-1]
+
     # keep only the digits in table2 and turn them into ints
     table2 = table2.applymap(lambda x: int("".join([y for y in x if y.isdigit()])))
 
@@ -36,10 +42,31 @@ def dataprep(data):
     table2["Destination"] = table2[0].apply(lambda x: int(str(x)[len(str(x)) - 1]))
     table2 = table2.iloc[:, 1:]
 
+    # rename the rows in table2 to ascending numbers starting from 0
+    table2.index = range(len(table2))
 
-    print(table1, table2)
+    return table1, table2
+
+
+def move_crates(position, instructions):
+
+    for i in range(len(instructions)):
+        for j in range(instructions["Amount"][i]):
+
+
+            dest_col = instructions["Destination"][i]
+            ori_col = instructions["Origin"][i]
+
+
+            dest_row = len(position)-len(position[position[dest_col] != " "]) - 1
+            ori_row = len(position)-len(position[position[ori_col] != " "])
+
+            position[dest_col][dest_row] = \
+                position[ori_col][ori_row]
+
+            position[ori_col][ori_row] = " "
+            print(position)
 
 
 
-
-dataprep("Test.txt")
+move_crates(dataprep("Test.txt")[0], dataprep("Test.txt")[1])
